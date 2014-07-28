af4e8eeeb780af7e378fc4d8bf8e3fe1e289dae0
dominikdz <dominikdz@users.noreply.github.com>
2014-07-28 21:29:56 +0200

    version update

--- ahh.sh
+++ ahh.sh
@@ -13,7 +13,7 @@ if [ ! $AHH_PATH_DEV = "" ] ; then
     echo "export AHH_PATH_DEV="
 fi
 REPO_PATH=https://github.com/dominikdz/ahh.git
-VERSION="1.0.10"
+VERSION="1.0.11"
 
 function run {
     echo "run here..."
